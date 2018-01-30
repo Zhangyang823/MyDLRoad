18766338538d6c96990e94aa59fcf973ec740881
“hhzh <huzihao79@163.com>
2018-01-30 23:19:21 +0800

    .

--- 1111.py
+++ 1111.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 a = np.ones((2,3,4,4))
-m = np.arange(16).reshape((2,2,2,2))
-x = np.rot90(m,2, (2,3))
-print(x)
+m = np.arange(16).reshape((4,4))
+# x = np.rot90(m,2, (2,3))
+print(m)
+print(m[1:-1,1:-1])
